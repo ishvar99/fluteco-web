491b0f6fa497a085e3801843bdb622905d340706
ishvar99 <ishanvarshney99@gmail.com>
2020-12-09 02:00:17 +0530

    resolved add to cart issue when limit exceeded

--- backend/controllers/cart.js
+++ backend/controllers/cart.js
@@ -1,47 +1,40 @@
 const asyncHandler = require('../middlewares/asyncHandler');
 const Cart = require('../models/cart')
-const Product =require('../models/product')
+const Product =require('../models/product');
 exports.displayCart=asyncHandler(async (req,res,next)=>{
-const cart =await Cart.find({}).populate('cartItems.product').exec()
-res.json(cart);
-})
+    const cart =await Cart.find({user:req.currentUser.id}).populate('cartItems.product').exec()
+    res.json(cart);
+ })
 
 exports.addToCart =asyncHandler(async (req,res,next)=>{
- let userCart = await Cart.findOne({user:req.currentUser.id})
-  if(!userCart)
+ let currentUserCart = await Cart.findOne({user:req.currentUser.id})
+  if(!currentUserCart)
   {
-    // create new cart
+  // cart does not exist for current user, create new cart
   newCart = await Cart.create({
     user:req.currentUser,
     cartItems:[req.body]
    })
    res.json(newCart)
+   return;
   }
-  if(userCart.cartItems.filter((e)=>e.product.toString()===req.body.product).length===0){
-    console.log('hello')
-    let updatedCart =await userCart.update({
+  let foundProduct=currentUserCart.cartItems.filter((e)=>e.product.toString()===req.body.product)[0];
+  if(!foundProduct){
+    // Product doesn't exist in the cart
+    let updatedCart =await currentUserCart.updateOne({
       "$push":{
        cartItems:req.body
       },
-     })
+     },{new:true})
      res.json(updatedCart)
   }
-  
    else{
      const product=await Product.findById(req.body.product);
      const stockCount =product.countInStock;
-    let cartProduct =await Cart.findOne({user:req.currentUser.id,'cartItems.product':req.body.product});
-    let actualQuantity;
-    let productQuantity=cartProduct.cartItems[0].qty;
-    if(productQuantity+req.body.qty >stockCount){
-      actualQuantity=stockCount-productQuantity;
-    }
-    else{
-      actualQuantity=req.body.qty;
-    }
-    console.log("New actualQuantity",actualQuantity);
+    const productQuantity=foundProduct.qty;
+    const quantity=parseInt(req.body.qty)
    let updatedProduct= await Cart.findOneAndUpdate({user:req.currentUser.id,'cartItems.product':req.body.product},
-   {$inc:{'cartItems.$.qty':actualQuantity}})
+   {$inc:{'cartItems.$.qty':productQuantity+quantity>stockCount?stockCount-productQuantity:quantity}})
     res.json(updatedProduct);
    }
 })
--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -2,12 +2,10 @@ const asyncHandler = require('../middlewares/asyncHandler')
 const Product=require('../models/product')
 exports.getProducts=asyncHandler(async (req,res)=>{
  const products=await Product.find({})
- // throw new Error('some error')
  res.json(products)
 })
 exports.getProduct=asyncHandler(async(req,res)=>{
  const product=await Product.findById(req.params.id)
- console.log(product)
  if(product)
  res.json(product)
  else{
